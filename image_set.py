c1eeb9eccb23e54a167383db9d574f4c354cf1ca
ousquid-share <ousquid@gmail.com>
2017-12-24 07:45:20 +0000

    Test split

--- image_set.py
+++ image_set.py
@@ -12,27 +12,20 @@ import math
 
 CacheMode = Enum("CacheMode", "use rewrite ignore")
 
-class NumazuShineException(Exception):
-    def __init__(self, str):
-        self.str = str
-    
-    def __str__(self):
-        return "NumazuShineException {}".format(self.str)
-
 class ImageSet(object):
     def __init__(self, x_data, y_data, num_classes, grayscale):
         self.x_data = x_data
         if len(x_data.shape) != 4:
-            raise NumazuShineException("shape of x_data ({}) is invalid".format(x_data.shape))
+            raise ValueError("shape of x_data ({}) is invalid".format(x_data.shape))
 
         self.y_data = y_data
         if len(y_data.shape) != 2:
-            raise NumazuShineException("shape of y_data ({}) is invalid".format(y_data.shape))
+            raise ValueError("shape of y_data ({}) is invalid".format(y_data.shape))
 
         if x_data.shape[0] != y_data.shape[0]:
             fmt = "unmatched x_data.shape[0] ({}) and y_data.shape[0] ({})"
             msg = fmt.format(x_data.shape[0], y_data.shape[0])
-            raise NumazuShineException(msg)
+            raise ValueError(msg)
         self.num_classes = num_classes
         self.color = 1 if grayscale else 3
 
@@ -88,6 +81,9 @@ class ImageSet(object):
         return tmp_inst
 
     def split(self, train_rate=0.9, seed=None):
+        if train_rate < 0 or train_rate > 1:
+            raise ValueError("invalid train_rate {}".format(train_rate))
+            
         # init rand mod.
         random.seed(seed)
         
--- tests/test_image_set.py
+++ tests/test_image_set.py
@@ -17,12 +17,64 @@ class TestImageSet(unittest.TestCase):
         
         # x_data と y_data の要素数が一致しない場合
         lack_ydata = np.zeros((9, 3))
-        with self.assertRaises(NumazuShineException):
+        with self.assertRaises(ValueError):
             ImageSet(ok_xdata, lack_ydata, num_classes, grayscale)
         
         # x_data, y_data で誤った次元数のデータを入れた場合
         ng_xdata = np.zeros((10))
         ng_ydata = np.zeros((10))
-        with self.assertRaises(NumazuShineException):
+        with self.assertRaises(ValueError):
             ImageSet(ng_xdata, ok_ydata, num_classes, grayscale)
             ImageSet(ok_xdata, ng_ydata, num_classes, grayscale)
+        
+
+    def test_split(self):
+        x_data = np.zeros((10, 128, 256, 3))
+        y_data = np.zeros((10, 3))
+        for i in range(10):
+            x_data[i].fill(i)
+            y_data[i].fill(i)
+        
+        num_classes = 3
+        grayscale = True
+        s = ImageSet(x_data, y_data, num_classes, grayscale)
+        
+        # train_rate に合ったデータ数を分割する
+        s.split(train_rate=0.9)
+        self.assertEqual(len(s.x_train), 9)
+        self.assertEqual(len(s.x_test), 1)
+        
+        s.split(train_rate=0.6)
+        self.assertEqual(len(s.x_train), 6)
+        self.assertEqual(len(s.x_test), 4)
+        
+        # seed が異なる場合に違う分割を行う
+        s.split(seed="first_seed")
+        x_test_first = s.x_test
+        s.split(seed="second_seed")
+        x_test_second = s.x_test
+        self.assertFalse(np.all(x_test_first == x_test_second))
+        
+        # seed を与えなかった場合に毎回違う分割が行われる
+        s.split(train_rate=0.9)
+        x_test_tmp = s.x_test
+        s.split(train_rate=0.9)
+        self.assertFalse(np.all(x_test_tmp == s.x_test))
+        
+        # train と test にダブリがない
+        s.split(train_rate=0.5)
+        for data in s.x_train:
+            self.assertNotIn(data, s.x_test)
+        
+        # trainとtestのxとyが対応している
+        for i in range(len(s.x_train)):
+            self.assertEqual(s.x_train[i][0][0][0], s.y_train[i][0])
+        for i in range(len(s.x_test)):
+            self.assertEqual(s.x_test[i][0][0][0], s.y_test[i][0])
+            
+        # train_rate に間違った値を入れたときに例外を返す
+        s.split(train_rate=0.0001)
+        s.split(train_rate=1)
+        with self.assertRaises(ValueError):
+            s.split(train_rate=0)
+            s.split(train_rate=1.0001)
